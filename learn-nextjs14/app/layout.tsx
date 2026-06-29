c3453bed551d12f5e225f757668829b86634be8b
Hakyu Lee <dlgkrbek123@gmail.com>
2026-06-29 11:37:29 +0900

    인트로

---
+++ learn-nextjs14/app/layout.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <div>
+          <main>{children}</main>
+        </div>
+      </body>
+    </html>
+  );
+}
